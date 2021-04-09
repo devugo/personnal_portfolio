922a0fce4d575af7649b2a208710dd23675bd712
Ezenwankwo Ugochukwu <ugoezenwankwo@gmail.com>
2021-04-09 22:51:33 +0100

    Add work experience to about section

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -9,36 +9,142 @@ const About = () => {
             <div className="about-container wrapper">
                 <h3 className="about-container__title short-underline section-title" id="about-container__title">ABOUT ME</h3>
                 <div className="about-container__content" id="about-container__content">
-                    <div className="about-container__content-img center-content">
-                        <img src={AboutImg2} className="top-in-shake" alt="My profile"/>
-                    </div>
-                    <div className="about-container__content-description">
-                        <div className="summary">
-                            <p>I am Ezenwankwo Ugochukwu or better still Devugo, a software engineer
-                                with three years of experience, pretty good with developing, building,
+                    <div className="about-container__content-bio">
+                        <div className="about-container__content-bio__img center-content">
+                            <img src={AboutImg2} className="top-in-shake" alt="My profile"/>
+                        </div>
+                        <div className="about-container__content-bio__summary center-content">
+                            <p>I am Ugochukwu Ezenwankwo or better still Devugo, a software developer
+                                with three years of professional experience, pretty good with developing, building,
                                 and designing web pages, applications across various platforms.
                             </p>
-                            <p>
-                                I'm great in the use of these technologies; HTML, CSS, PHP, Javascript, 
-                                React, Symfony, Laravel and SQL. Ease of use and customer satisfaction
-                                 are pretty much my primary goal in the entire development processes.
-                            </p>
-                            <p>
-                                In my years of development, i have acquired and utilized these skill 
-                                set in various project conducted; in both frontend and back end development, 
-                                design of various web pages and application, building robust APIs and scalable applications.
-                            </p>
+                            <div className="skills-icons">
+                                <Tag className="tag-me" color="purple">HTML</Tag>
+                                <Tag className="tag-me" color="blue">CSS</Tag>
+                                <Tag className="tag-me" color="magenta">Javascript</Tag>
+                                <Tag className="tag-me" color="red">React Js</Tag>
+                                <Tag className="tag-me" color="volcano">React Native</Tag>
+                                <Tag className="tag-me" color="orange">PHP</Tag>
+                                <Tag className="tag-me" color="gold">Symfony</Tag>
+                                <Tag className="tag-me" color="green">Laravel</Tag>
+                                <Tag className="tag-me" color="yellow">SQL</Tag>
+                            </div>
                         </div>
-                        <div className="skills-icons">
-                            <Tag className="tag-me" color="purple">HTML</Tag>
-                            <Tag className="tag-me" color="blue">CSS</Tag>
-                            <Tag className="tag-me" color="magenta">Javascript</Tag>
-                            <Tag className="tag-me" color="red">React Js</Tag>
-                            <Tag className="tag-me" color="volcano">React Native</Tag>
-                            <Tag className="tag-me" color="orange">PHP</Tag>
-                            <Tag className="tag-me" color="gold">Symfony</Tag>
-                            <Tag className="tag-me" color="green">Laravel</Tag>
-                            <Tag className="tag-me" color="yellow">SQL</Tag>
+                    </div>
+                    <div className="about-container__content-resume">
+                        <div className="resume-title">
+                            <h3>WORK EXPERIENCE</h3>
+                        </div>
+                        <div className="work-history">
+                            <div className="single-work">
+                                <div className="single-work__title">
+                                    <div className="position-organization">
+                                        <h4 className="position">Software Developer</h4>
+                                        <h5 className="organization">Octosoft Technologies</h5>
+                                    </div>
+                                    <div className="date-period-container">
+                                        <p className="date-period">Feb 2021 - Till date</p>
+                                    </div>
+                                </div>
+                                <div className="job-description">
+                                    <div className="responsiblities">
+                                        <h5>Responsibilities</h5>
+                                        <ul>
+                                            <li>Developed mobile app utilizing React native cross platform capabilities to build for both platforms.</li>
+                                            <li>Developed robust APIs and applications.</li>
+                                            <li>Convert UI designs and templates to code.</li>
+                                        </ul>
+                                    </div>
+                                    <div className="accomplishemnts">
+                                        <h5>Achievements</h5>
+                                        <ul>
+                                            <li>Built a mobile expense app for the parent group to manage expenses within the organization.</li>
+                                        </ul>
+                                    </div>
+                                    <div className="tools">
+                                        <h5>Tools</h5>
+                                        <ul>
+                                            <li>HTML, CSS, Javascript, React, React Native, Strapi JS, Node JS, Docker, AWS etc.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="single-work">
+                                <div className="single-work__title">
+                                    <div className="position-organization">
+                                        <h4 className="position">Fullstack Developer</h4>
+                                        <h5 className="organization">Emplug Ltd.</h5>
+                                    </div>
+                                    <div className="date-period-container">
+                                        <p className="date-period">Jun 2019 - Jan 2021</p>
+                                    </div>
+                                </div>
+                                <div className="job-description">
+                                    <div className="responsiblities">
+                                        <h5>Responsibilities</h5>
+                                        <ul>
+                                            <li>Design the overall architecture of applications.</li>
+                                            <li>Responsible for building UIs and Client's side of an application.</li>
+                                            <li>Developing restfula dn graphql APIs</li>
+                                            <li>Testing, troubleshooting and debugging of applications.</li>
+                                            <li>Fixing and maintaining of existing applications.</li>
+                                        </ul>
+                                    </div>
+                                    <div className="accomplishemnts">
+                                        <h5>Achievements</h5>
+                                        <ul>
+                                            <li>I was involved in the development of a Computer Based Test (CBT) application which we used all over 
+the nation in conducting large scale recruitment exercises like that of the NDLEA</li>
+                                            <li> I was also involved in the development of a 
+job application portal; for the management of recruitment processes (the jobs and applications). Being a full stack developer
+got me involved in both tiers of development i.e. Front end and Back end</li>
+                                        </ul>
+                                    </div>
+                                    <div className="tools">
+                                        <h5>Tools</h5>
+                                        <ul>
+                                            <li>HTML, CSS, Javascript, React, PHP, Symfony, Firebase etc.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="single-work">
+                                <div className="single-work__title">
+                                    <div className="position-organization">
+                                        <h4 className="position">Web Developer (Freelance)</h4>
+                                        <h5 className="organization">Upwork and Fiverr.</h5>
+                                    </div>
+                                    <div className="date-period-container">
+                                        <p className="date-period">Feb 2018 - Jan 2019</p>
+                                    </div>
+                                </div>
+                                <div className="job-description">
+                                    <div className="responsiblities">
+                                        <h5>Responsibilities</h5>
+                                        <ul>
+                                            <li>Responsible for designing, coding and modifying websites, from layout to function according to a client's specifications.</li>
+                                            <li> Create visually appealing sites that feature user-friendly design and clear navigation.
+</li>
+                                            <li>Fixing problems within existing projects etc</li>
+                                        </ul>
+                                    </div>
+                                    <div className="accomplishemnts">
+                                        <h5>Achievements</h5>
+                                        <ul>
+                                            <li>Developed a multi school <a href="https://www.resultdirect.org/" target="_blank" rel="noreferrer">result checker</a> application for managing results in schools.</li>
+                                            <li>Built a <a href="https://brawtavacations.com/" target="_blank" rel="noreferrer">sales commission managment </a> application for managing sales and commission of affiliates.</li>
+                                        </ul>
+                                    </div>
+                                    <div className="tools">
+                                        <h5>Tools</h5>
+                                        <ul>
+                                            <li>HTML, CSS, Javascript, React, PHP, Laravel etc.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="download">
                             <a className="btn" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1vMHHTqo0BCbs6_om1-olSorcDqwq1dkL/view?usp=sharing">VIEW CV</a>
